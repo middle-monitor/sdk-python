8c466fa260d55d9bb8bcbc070742e2628bb892eb
ismail reida <ismailrei@gmail.com>
2026-07-19 22:52:36 +0200

    fix: stop auto-init without a token, add 5xx reporting opt-out

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="middle-monitor-sdk",
-    version="0.1.4",
+    version="0.1.5",
     description="Python SDK for Middle-Monitor error reporting with OpenTelemetry",
     author="Middle-Monitor",
     license="MIT",
